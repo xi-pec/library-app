93c2c15c21a42f8b53373b5b35c602fe303f4e8a
xi_pec <xepicscripter@gmail.com>
2025-08-06 09:22:21 +0800

    chore: add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xipec.library',
+  appName: 'library-app',
+  webDir: 'dist'
+};
+
+export default config;
