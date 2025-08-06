dfd42c457e3f1a6efaac6fca8b0551010cca4af0
xi_pec <xepicscripter@gmail.com>
2025-08-06 16:44:53 +0800

    chore: rename app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.xipec.library',
-  appName: 'library-app',
+  appName: 'CBZRC Library',
   webDir: 'dist',
 
   plugins: {
