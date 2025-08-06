490c8e0b38d0319f8e5a9a4e1ef90c9eebb487d6
xi_pec <xepicscripter@gmail.com>
2025-08-06 18:18:59 +0800

    feat: implement ui and functionality for welcome page

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,7 +1,23 @@
 import DefaultLayout from "@/layouts/default";
 
+import { Button } from "@heroui/button"
+import { Link } from "@heroui/link"
+
 export default function WelcomePage() {
     return <DefaultLayout>
-        <h1>Welcome!</h1>
+        <div className="h-full flex flex-col">
+            <div className="grow shrink basis-auto">
+                <h1 className="text-4xl font-bold">Welcome!</h1>
+                
+                <br />
+
+                <p className="text-default-800">
+                    To get started, enter your information in the next page. 
+                    This can be updated later.
+                </p>
+            </div>
+            
+            <Button href="/information" as={Link} className="w-full" color="primary">Go</Button>
+        </div>
     </DefaultLayout>
 }
