d3177d91526b8d7aad235a7dbc84d438934afda8
xi_pec <xepicscripter@gmail.com>
2025-08-06 19:51:50 +0800

    chore: switch to real form constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,23 +1,10 @@
-// REAL CONSTANTS
-// export default {
-//   formId: "1FAIpQLSeZ981_T13PIMg2xKPARjuMgQU-Fh1EEra7D1Yh_PYTzQtItQ",
-//   email: "emailAddress",
-//   name: "entry.1563336077",
-//   role: "entry.16782952",
-//   organization: "entry.1020791105",
-//   temperature: "entry.1784042296",
-//   hour_in: "entry.2098260905_hour",
-//   mins_in: "entry.2098260905_minute"
-// }
-
-// FAKE CONSTANTS
 export default {
-  formId: "1FAIpQLSebpb35NduxtbiXd3j2tkxem4sHKWCZzsPI-2tQwSOs7dN92A",
+  formId: "1FAIpQLSeZ981_T13PIMg2xKPARjuMgQU-Fh1EEra7D1Yh_PYTzQtItQ",
   email: "emailAddress",
-  name: "entry.1213408220",
-  role: "entry.1523034486",
-  organization: "entry.1800708313",
-  temperature: "entry.974719816",
-  hour_in: "entry.2106352473_hour",
-  mins_in: "entry.2106352473_minute",
-};
+  name: "entry.1563336077",
+  role: "entry.16782952",
+  organization: "entry.1020791105",
+  temperature: "entry.1784042296",
+  hour_in: "entry.2098260905_hour",
+  mins_in: "entry.2098260905_minute"
+}
